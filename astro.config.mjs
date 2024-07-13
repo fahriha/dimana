e21d9a8190700d5da24cd438fef13ec30e6026ed
nonspace <143269979+nowspace@users.noreply.github.com>
2024-07-14 01:41:22 +0700

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import bookshop from '@bookshop/astro-bookshop';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://top-quail.cloudvent.net/",
-  integrations: [bookshop(), react()]
-});
+  site: 'https://dimana.github.io',
+  base: 'my-repo',
+})
